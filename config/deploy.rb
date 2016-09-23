12e5c90560d1234c928b3bc53e14e4aa30fb2dfa
René Sprotte <rene.sprotte@provideal.net>
2016-09-23 10:58:31 +0200

    Add capistrano deployment.

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+lock "3.6"
+
+set :application, "shelf_finder"
+set :repo_url,    "git@github.com:ubpb/shelf_finder.git"
+set :branch,      "master"
+set :log_level,   :debug
+
+set :linked_files, fetch(:linked_files, []).push(
+  "config/database.yml", "config/secrets.yml"
+)
+set :linked_dirs, fetch(:linked_dirs, []).push(
+  "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system"
+)
+
+set :rvm_type,         :user
+set :rvm_ruby_version, "default"
+
+set :rails_env, "production"
+
+namespace :deploy do
+  after :publishing, :restart_app do
+    on roles(:web) do
+      within release_path do
+        execute :touch, "tmp/restart.txt"
+      end
+    end
+  end
+end
---
+++ config/deploy/testing.rb
@@ -0,0 +1,2 @@
+server "ubstage1.upb.de", user: "ubpb", roles: %w{app db web}
+set :deploy_to, "/home/ubpb/shelf-finder"
