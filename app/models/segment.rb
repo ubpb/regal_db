1b637c51a85c20c993c6e445a9d68b07f2073416
René Sprotte <rene.sprotte@provideal.net>
2016-11-16 14:10:16 +0100

    Update constants.

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,5 +1,8 @@
 class Segment < ApplicationRecord
 
+  SHELF_WIDTHS = [50, 75, 100, 120]
+  NO_OF_LEVELS = 1..10
+
   belongs_to :shelf
 
   validates :identifier, presence: true, numericality: {greater_than: 0, only_integer: true}, uniqueness: {scope: :shelf}
