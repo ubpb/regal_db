3e04301b4620bc042ac8b03a22d5f2a40dc45465
René Sprotte <renspr@gmail.com>
2017-05-10 14:33:41 +0200

    Validate presence of display_name.

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,5 +3,6 @@ class Location < ApplicationRecord
   has_many :shelves, -> { order(:identifier) }, dependent: :destroy
 
   validates :identifier, presence: true, numericality: {greater_than: 0, only_integer: true}, uniqueness: true
+  validates :display_name, presence: true
 
 end
