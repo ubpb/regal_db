8b8169e6118e7b9e0f8b3ff3af1b5f0dbed9adaf
René Sprotte <renspr@gmail.com>
2017-06-01 11:15:58 +0200

    Update deployment.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server "ubperseus", user: "ubpb", roles: %w{app web db}
-server "ubpegasus", user: "ubpb", roles: %w{app web}
-set :branch, "production"
-set :deploy_to, "/ubpb/shelf-finder"
+# server "ubperseus", user: "ubpb", roles: %w{app web db}
+# server "ubpegasus", user: "ubpb", roles: %w{app web}
+# set :branch, "production"
+# set :deploy_to, "/ubpb/shelf-finder"
--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,2 +1,2 @@
 server "ubstage1", user: "ubpb", roles: %w{app db web}
-set :deploy_to, "/home/ubpb/shelf-finder"
+set :deploy_to, "/home/ubpb/regaldb"
