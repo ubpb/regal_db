2ae8999d1022dc5948717e438092e25cc34b958d
René Sprotte <renspr@gmail.com>
2017-06-26 14:31:20 +0200

    Set production deployment.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
-# server "ubperseus", user: "ubpb", roles: %w{app web db}
-# server "ubpegasus", user: "ubpb", roles: %w{app web}
-# set :branch, "production"
-# set :deploy_to, "/ubpb/shelf-finder"
+server "ubperseus", user: "ubpb", roles: %w{app web db}
+server "ubpegasus", user: "ubpb", roles: %w{app web}
+set :branch, "production"
+set :deploy_to, "/ubpb/regaldb"
+
+set :rvm_ruby_version, "2.4.1"
