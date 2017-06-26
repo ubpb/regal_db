09f7b7e6c72978a70e63cc0dbb1ab78fdd7baa13
René Sprotte <renspr@gmail.com>
2017-06-26 15:28:10 +0200

    2.4.1 is default now.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,3 @@ server "ubperseus", user: "ubpb", roles: %w{app web db}
 server "ubpegasus", user: "ubpb", roles: %w{app web}
 set :branch, "production"
 set :deploy_to, "/ubpb/regaldb"
-
-set :rvm_ruby_version, "2.4.1"
