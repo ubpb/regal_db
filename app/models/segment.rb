603e8a146904f1cbe1d3459f0f3d34b502437a2f
René Sprotte <renspr@gmail.com>
2017-08-29 08:41:37 +0200

    Remove wrong validations.

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -7,8 +7,8 @@ class Segment < ApplicationRecord
 
   validates :identifier, presence: true, numericality: {greater_than: 0, only_integer: true}, uniqueness: {scope: :shelf}
   validates :utilisation, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 1}
-  validates :width, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 100, only_integer: true}
-  validates :no_of_levels, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 10, only_integer: true}
+  validates :width, presence: true, numericality: {greater_than: 0, only_integer: true}
+  validates :no_of_levels, presence: true, numericality: {greater_than: 0, only_integer: true}
   validates :interval_begin, presence: true
   validates :interval_end, presence: true
 
