1b754b1b22995f00d938b764d6e20e94c93ff5d7
René Sprotte <renspr@gmail.com>
2017-08-30 13:43:37 +0200

    Make find_segments public and able to find ranges.

--- app/services/shelf_finder.rb
+++ app/services/shelf_finder.rb
@@ -5,20 +5,23 @@ class ShelfFinder
     Result.new(segments)
   end
 
+  def find_segments(code_begin, code_end = nil)
+    code_end = code_begin unless code_end
+
+    Segment.includes(
+      :shelf => :location
+    ).where(
+      "interval_begin <= :code_begin AND interval_end >= :code_end",
+      code_begin: normalize_code(code_begin),
+      code_end: normalize_code(code_end)
+    )
+  end
+
 private
 
   def normalize_code(code)
     norm_code = code[/[a-z]{3,4}/i] || code
     norm_code&.upcase
-  end
-
-  def find_segments(code)
-    Segment.includes(
-      :shelf => :location
-    ).where(
-      "interval_begin <= :code AND interval_end >= :code",
-      code: normalize_code(code)
-    )
   end
 
 end
