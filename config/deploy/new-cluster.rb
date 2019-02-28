6ae29961f968f3bb588b52ace7bd50d25a564c1c
René Sprotte <renspr@gmail.com>
2019-02-28 11:42:08 +0100

    Set brach for new cluster dpeloyment.

--- config/deploy/new-cluster.rb
+++ config/deploy/new-cluster.rb
@@ -1,4 +1,4 @@
 server "ubpegasus18", user: "ubpb", roles: %w{app web db}
 #server "ubperseus18", user: "ubpb", roles: %w{app web}
-set :branch, "production"
+set :branch, "new-cluster"
 set :deploy_to, "/ubpb/regaldb"
