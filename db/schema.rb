acfbfed053e42a8dd9f061ab28c746f2fc5b72df
René Sprotte <renspr@users.noreply.github.com>
2022-11-03 10:39:42 +0100

    Upgrade to Rails 7.

--- db/schema.rb
+++ db/schema.rb
@@ -2,17 +2,16 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_111453) do
-
-  create_table "locations", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_092855) do
+  create_table "locations", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.integer "identifier", null: false
     t.string "display_name"
     t.boolean "closed_stack", default: false, null: false
@@ -21,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_10_06_111453) do
     t.index ["identifier"], name: "index_locations_on_identifier", unique: true
   end
 
-  create_table "segments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "segments", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.integer "shelf_id", null: false
     t.integer "identifier", null: false
     t.string "interval_begin"
@@ -31,19 +30,19 @@ ActiveRecord::Schema.define(version: 2021_10_06_111453) do
     t.integer "no_of_levels"
     t.text "notes"
     t.string "special_usage"
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["identifier", "shelf_id"], name: "index_segments_on_identifier_and_shelf_id", unique: true
     t.index ["interval_begin"], name: "index_segments_on_interval_begin"
     t.index ["interval_end"], name: "index_segments_on_interval_end"
     t.index ["shelf_id"], name: "index_segments_on_shelf_id"
   end
 
-  create_table "shelves", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+  create_table "shelves", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.integer "location_id", null: false
     t.integer "identifier", null: false
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["identifier", "location_id"], name: "index_shelves_on_identifier_and_location_id", unique: true
     t.index ["location_id"], name: "index_shelves_on_location_id"
   end
