050e8fd2124fc9b45f5cc4797706d0911be6d05c
René Sprotte <renspr@users.noreply.github.com>
2024-09-13 11:44:07 +0200

    Add app1 and app2.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
-server "ubpegasus18.upb.de", user: "ubpb", roles: %w{app web db}
+server "ubpegasus18.upb.de", user: "ubpb", roles: %w{app web}
 server "ubperseus18.upb.de", user: "ubpb", roles: %w{app web}
+server "app1.ub.upb.de",     user: "ubpb", roles: %w{app web db}
+server "app2.ub.upb.de",     user: "ubpb", roles: %w{app web}
 set :branch, "production"
 set :deploy_to, "/ubpb/regaldb"
